0abccf24e25f92ff53ba7aab58f0046b57d6fa38
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-04-04 21:15:42 +0530

    more pages added!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ImagesComponent } from './pages/images/images.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"images",component:ImagesComponent},
+  {path:"films",component:FilmsComponent},
+  {path:"aboutUs",component:AboutComponent},
+  {path:"contactUs",component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ImagesComponent } from './pages/images/images.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ImagesComponent,
+    FilmsComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
